bdb440e9bbab3778dc09f24d617d0dba52535add
sche34 <timo.scheidel@live.nl>
2024-05-31 14:19:46 +0200

    add distance_matrix & prepare data on initialization

--- similarity_calculation/main.py
+++ similarity_calculation/main.py
@@ -1,16 +1,19 @@
 import geopandas as gpd
+import numpy as np
+
 from sklearn.metrics.pairwise import euclidean_distances
 
 class similarity:
     def __init__(self, gpkg_path: str, column_weights: dict = None, columns: list = None):
         self.gpkg_path = gpkg_path
-        self.gpf = gpd.read_file(self.gpkg_path)
-        self.prepared_gpf = None
+        self.gpdf = gpd.read_file(self.gpkg_path)
 
         self._validate_input(column_weights, columns)
 
         self.column_weights = column_weights
         self.columns = self._get_columns(column_weights, columns)
+
+        self.prepare_data()
     
     ## helper functions for __init__
     def _validate_input(self, column_weights, columns):
@@ -23,27 +26,26 @@ class similarity:
         if column_weights is not None:  # overwrite columns if column_weights is given
             return list(column_weights.keys())
         elif columns is None and column_weights is None:
-            return self.gpf.columns.drop(["id", "geometry"])
+            return self.gpdf.columns.drop(["id", "geometry"])
         else:
             return columns
     
     ## main functions
     def normalize(self):
         """normalize the columns in the geopandas dataframe so that every feature has the same weight in the distance calculation"""
-        self.prepared_gpf = self.gpf.copy()
+        self.prepared_gpdf = self.gpdf.copy()
         for column in self.columns:
-            self.prepared_gpf[column] = (self.gpf[column] - self.gpf[column].mean()) / self.gpf[column].std()
+            self.prepared_gpdf[column] = (self.gpdf[column] - self.gpdf[column].mean()) / self.gpdf[column].std()
     
     def scale(self):
-        """scale the columns in the geopandas dataframe according to the given weights"""
+        """scale the columns in the geopandas dataframe according to the given weights, so that the features have different weights in the distance calculation"""
         for column, weight in self.column_weights.items():
-            self.prepared_gpf[column] = self.prepared_gpf[column] * weight
+            self.prepared_gpdf[column] = self.prepared_gpdf[column] * weight
 
     def prepare_data(self):
-        if self.prepared_gpf is None:
-            self.normalize()
-            if self.column_weights is not None:
-                self.scale()
+        self.normalize()
+        if self.column_weights is not None:
+            self.scale()
 
     def check_ids(self, id1, id2):
         # check if the ids are in the correct format
@@ -53,22 +55,20 @@ class similarity:
             id2 = f"NL.IMBAG.Pand.{id2}-0"
 
         # check if the ids are in the geopandas dataframe
-        if id1 not in self.prepared_gpf["id"].values:
+        if id1 not in self.prepared_gpdf["id"].values:
             raise ValueError(f"id1 {id1} not found in the geopandas dataframe")
-        if id2 not in self.prepared_gpf["id"].values:
+        if id2 not in self.prepared_gpdf["id"].values:
             raise ValueError(f"id2 {id2} not found in the geopandas dataframe")
         
         # if all good, return the ids
         return id1, id2
 
     def calculate_distance(self, id1, id2):
-        if self.prepared_gpf is None:
-            self.prepare_data()
 
         id1, id2 = self.check_ids(id1, id2)
         
-        obj1 = self.prepared_gpf[self.prepared_gpf["id"] == id1]
-        obj2 = self.prepared_gpf[self.prepared_gpf["id"] == id2]
+        obj1 = self.prepared_gpdf[self.prepared_gpdf["id"] == id1]
+        obj2 = self.prepared_gpdf[self.prepared_gpdf["id"] == id2]
 
         dist = euclidean_distances(obj1[self.columns], obj2[self.columns])
         return dist[0][0]
@@ -80,6 +80,44 @@ class similarity:
         else:
             normalized_dist = dist / len(self.columns)
         return 1 / (1 + normalized_dist)
+    
+    def distance_matrix(self, output_path: str = None, save_interval: int = 100):
+        if isinstance(output_path, str) and output_path.endswith('.csv') == False:
+            raise ValueError("output_path must end with '.csv'")
+
+        # create an empty matrix and get all ids
+        all_ids = self.prepared_gpdf["id"].values
+        header = ",".join(all_ids)
+        matrix = np.array([])
+
+        # calculate the distance between all objects
+        for id1 in all_ids:
+            row = np.array([])
+            for id2 in all_ids:
+                if id1 == id2:
+                    dist = 0
+                else:
+                    dist = sim.calculate_distance(id1, id2)
+                row = np.append(row, round(dist, 5))
+            
+            # add row to matrix
+            if matrix.size == 0:
+                matrix = row
+            else:
+                matrix = np.vstack((matrix, row))
+            
+            # save the matrix to a file if the interval is reached
+            if isinstance(output_path, str) and matrix.ndim > 1 and matrix.shape[0] % save_interval == 0:
+                np.savetxt(output_path, matrix, delimiter=",", fmt='%f', header=header, comments='')
+                #TODO: just append the new rows to the file instead of saving the whole matrix
+        
+        # make sure the full matrix is saved
+        if isinstance(output_path, str):
+            np.savetxt(output_path, matrix, delimiter=",", fmt='%f', header=header, comments='')
+            print("Distance matrix calculated and saved to 'distance_matrix.csv'")
+        else:
+            print("Distance matrix calculated")
+        return matrix
 
 if __name__ == '__main__':
     path = "collection/output/merged.gpkg"
@@ -95,3 +133,5 @@ if __name__ == '__main__':
 
     simi = sim.calculate_similarity(id1, id2)
     print('Similarity between the two objects is: ', simi)
+
+    sim.distance_matrix(output_path="distance_matrix.csv", save_interval=5)
