ee3ed0539ed916bec981b21ae6db1a855a2d9eca
sche34 <timo@scheidel.nl>
2024-07-24 15:48:36 +0200

    added progress bar for async requests

--- collection/async_requests.py
+++ collection/async_requests.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime
 import aiohttp
+from tqdm import tqdm
 
 ### Async requests
 async def fetch(session, task, headers, payload):  # fetching urls and mark result of execution
@@ -36,6 +37,15 @@ def extract_results(url_tasks):
 def tasks_to_wait(url_tasks):
     return len([i for i in url_tasks if i['status'] not in ['done', 'error']])
 
+def tasks_done(url_tasks):
+    return len([i for i in url_tasks if i['status'] in ['done', 'error']])
+
+def update_bar(t, previous_progress, url_tasks):
+    current_progress = tasks_done(url_tasks)
+    t.update(current_progress - previous_progress)
+    previous_progress = current_progress
+    return previous_progress
+
 async def request_url_list(all_urls, headers={}, payload={}, persecond=50):
     async with aiohttp.ClientSession() as session:
         # convert to list of dicts
@@ -43,8 +53,14 @@ async def request_url_list(all_urls, headers={}, payload={}, persecond=50):
 
         started_tasks_this_second = 0
         current_second = datetime.now().second
+        
+        t = tqdm(total=len(url_tasks), desc='Requesting data...')
+        previous_progress = 0
+
         for index, task in enumerate(url_tasks):
             if started_tasks_this_second > persecond:
+                previous_progress = update_bar(t, previous_progress, url_tasks)
+                
                 # wait until the next second so we can start new tasks
                 time_to_next_second = 1 - (datetime.now().microsecond / 1000000)
                 await asyncio.sleep(time_to_next_second)
@@ -62,6 +78,7 @@ async def request_url_list(all_urls, headers={}, payload={}, persecond=50):
         # loop until all tasks are done or error
         while tasks_to_wait(url_tasks) != 0:
             await asyncio.sleep(0.1)
+            previous_progress = update_bar(t, previous_progress, url_tasks)
 
     # return results
     all_results = extract_results(url_tasks)
