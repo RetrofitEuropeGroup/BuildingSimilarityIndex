38271b4c25b91549bbdcaa346046cc7f49846428
sche34 <timo@scheidel.nl>
2024-07-24 14:18:40 +0200

    added more documentation

--- processing/main.py
+++ processing/main.py
@@ -11,27 +11,39 @@ from metrics.cityStats import calculate_metrics
 from turning_functions.main import process_to_features
 
 class processing():
-    def __init__(self, output_path: str, bag_data_folder: str = None, cityjson_path: str = None):
-        # TODO: add documentation about the variables
-        if bag_data_folder is None and cityjson_path is None:
-            raise ValueError("Either input_folder or cityjson_path should be provided")
-        elif bag_data_folder is not None and cityjson_path is not None:
-            raise ValueError("It is not possible to use both bag_data_folder and cityjson_path")
+    def __init__(self, output_file: str, bag_data_folder: str = None, cityjson_file: str = None):
+        """
+        Initializes an instance of the class that is used to process the data. Processing the data consists of 
+        two steps: merging the files in the input folder to a single file (1) and creating a feature space from the merged file.
+        The feature space is a pandas dataframe that contains the 2D and 3D metrics and the turning function features, other
+        variables could be added as well. It saves the feature space to a csv file in the output_file and saves it to self.feature_space
+
+        Args:
+            output_file (str): The path to the output file where the processed data (feature space) will be saved.
+            bag_data_folder (str, optional): The path to the folder containing cityjson with a single building, source is from the BAG. Defaults to None.
+            cityjson_file (str, optional): The path to the CityJSON file with a single or multiple buildings. Defaults to None.
+        """
 
         self.bag_data_folder = bag_data_folder
-        self.cityjson_path = cityjson_path
-        self.validate_output_path(output_path)
+        self.cityjson_file = cityjson_file
+        self.set_output_file(output_file)
 
+        if bag_data_folder is None and cityjson_file is None:
+            raise ValueError("Either input_folder or cityjson_file should be provided")
+        elif bag_data_folder is not None and cityjson_file is not None:
+            raise ValueError("It is not possible to use both bag_data_folder and cityjson_file")
 
-    def validate_output_path(self, file_path: str):
+        
+
+    def set_output_file(self, file_path: str):
         if file_path.endswith('.csv') == False:
-            raise ValueError("""The output_path can only be a .csv file""")
+            raise ValueError("The output_file can only be a .csv file")
         else:
             parent_dir = os.path.dirname(file_path)
-            if not os.path.exists(parent_dir):
+            if not os.path.exists(parent_dir) and parent_dir != '': # if the parent directory does not exist, create it
                 os.makedirs(parent_dir)
 
-            self.output_path = file_path        
+            self.output_file = file_path        
 
     # main functions
     def merge_files(self):
@@ -40,7 +52,7 @@ class processing():
         merger = MergeCityJSON(self.bag_data_folder, output_folder=merge_folder)
         merger.run()
 
-        self.cityjson_path = merger.file_path
+        self.cityjson_file = merger.file_path
 
     def create_feature_space(self):
         """ Create a pd dataframe with the feature space. First step in the process is to calculate the 2d / 3d metrics on the merged cityjson which outputs a geo df, this is then used to execute the turning function"""
@@ -48,14 +60,16 @@ class processing():
         n_processors = max(os.cpu_count() - 1, 1)
 
         # run the citystats script in which the metrics are calculated
-        feature_space_metrics = calculate_metrics(input=self.cityjson_path, jobs=n_processors)
+        feature_space_metrics = calculate_metrics(input=self.cityjson_file, jobs=n_processors)
 
         # execute the turning function
         feature_space_tf = process_to_features(feature_space_metrics)
 
         # merge the results in one data frame & return the df
-        feature_space_full = feature_space_metrics.merge(feature_space_tf, on='id')
-        return feature_space_full
+        feature_space_merged = feature_space_metrics.merge(feature_space_tf, on='id')
+        feature_space_merged.drop(columns=['geometry'], inplace=True) # drop the geometry column as it is a shapely object and cannot be saved to a csv file
+
+        return feature_space_merged
 
     def run(self):
         # merge if a folder has been provided
@@ -63,13 +77,13 @@ class processing():
             self.merge_files()
 
         # calculate the 2d / 3d metrics and turning funciton features
-        feature_space_full = self.create_feature_space()
-        feature_space_full.to_csv(self.output_path)
+        self.feature_space = self.create_feature_space()
+        self.feature_space.to_csv(self.output_file)
 
 if __name__ == '__main__':
-    p = processing(output_path="data/feature_space/test_fast.csv", bag_data_folder="data/bag_data")
+    p = processing(output_file="data/feature_space/test_fast.csv", bag_data_folder="data/bag_data")
     p.run()
     
     import pandas as pd
-    df = pd.read_csv(p.output_path)
+    df = pd.read_csv(p.output_file)
     print(df.head())
