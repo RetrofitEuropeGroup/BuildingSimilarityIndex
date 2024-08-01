281d6c127b7ea732cbddc29a146c1b070b363006
sche34 <timo@scheidel.nl>
2024-08-01 15:04:51 +0200

    make it robust for different id formats

--- collection/main.py
+++ collection/main.py
@@ -26,6 +26,16 @@ class collection():
         cityjson['vertices'] = cityjsonfeature['vertices']
         return cityjson
 
+    def _make_url(self, id: str):
+        return f"https://api.3dbag.nl/collections/pand/items/NL.IMBAG.Pand.{id}"
+
+    def convert_id(self, id: str):
+        if id.startswith("NL.IMBAG.Pand."): # remove the prefix if it is there
+            id = id[14:]
+        if '-' in id: # remove the suffix if it is there
+            id = id.split('-')[0]
+        return id
+
     def _save(self, cityjson: dict, id: str):
         """Saves the CityJSON data to a file."""
         with open(f'{self._bag_data_folder}/{id}.city.json', 'w') as file:
@@ -40,7 +50,6 @@ class collection():
         # else: the folder is there already
 
     def collect_id_list(self, all_ids: list):
-        # TODO: check if exists with different id formats
         """Requests and save the data in cityjson format for all the ids in the list."""
 
         self._check_bag_data_folder()
@@ -49,8 +58,7 @@ class collection():
         request_ids = []
         existing_files = 0
         for id in all_ids:
-            if id.startswith("NL.IMBAG.Pand."):
-                id = id[14:]
+            id = self.convert_id(id)
 
             if os.path.exists(f"{self._bag_data_folder}/{id}.city.json"):
                 existing_files += 1
@@ -58,32 +66,36 @@ class collection():
                 request_ids.append(id)
 
         # check how many requests are needed
+        print(existing_files, len(all_ids), request_ids)
         if existing_files == len(all_ids):
             print("All the requested data is already saved on the machine, no new requests are needed.")
             return
         elif existing_files > 0:
             print(f"{existing_files} out of {len(all_ids)} files already exist, so {len(all_ids) - existing_files} more request(s) are needed.")
-        
-        ## make the urls for the async requests
-        all_urls = [f"https://api.3dbag.nl/collections/pand/items/NL.IMBAG.Pand.{id}" for id in request_ids]
+
+        # if asyncio is running, do the requests synchronously
         if asyncio.get_event_loop().is_running():
-            print("Asyncio is already running, so the requests will be done synchronously. Note that this is not the most efficient way.")
-            for url in all_urls:
+            error_count = 0
+            if self._verbose:
+                print("Asyncio is already running, so the requests will be done synchronously. Note that this is not the most efficient way.")
+            for id in request_ids: # TODO: add a progress bar
                 try:
-                    r = requests.get(url)
+                    r = requests.get(self._make_url(id))
                     cityjson = self._convert_to_cityjson(r.json())
-                    self._save(cityjson, request_ids[all_urls.index(url)])
+                    self._save(cityjson, id)
                 except:
                     error_count += 1
             if self._verbose:
                 print(f"{error_count} errors occurred while requesting the data.")
+        # if asyncio is not running, use the async function
         else:
+            all_urls = [self._make_url(id) for id in request_ids]
             result = asyncio.run(request_url_list(all_urls))
 
             # convert to the right format and save the data, this is not async because the data is already fetched
             for i, data in enumerate(result):
                 cityjson = self._convert_to_cityjson(data)
-                self._save(cityjson, all_ids[i])
+                self._save(cityjson, request_ids[i]) # use request_ids to get the right id without the pre- and suffix
             if self._verbose:
                 print(f"{len(all_urls)-len(result)} errors occurred while requesting the data.")
         
