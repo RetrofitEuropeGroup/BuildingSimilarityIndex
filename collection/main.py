08f4dc8dba4cce20ac03721d43c5c83bf256a170
sche34 <timo.scheidel@live.nl>
2024-08-12 16:31:06 +0200

    split the class into smaller funcitons

--- collection/main.py
+++ collection/main.py
@@ -127,14 +127,8 @@ class collection():
             roof_attributes[id] = roofmetrics(id, bbox)
         return roof_attributes
 
-
-    def collect_id_list(self, all_ids: list, force_new=False):
-        """Requests and save the data in cityjson format for all the ids in the list."""
-
-        self._check_bag_data_folder()
-
-        # Remove the prefix from the ids so they are consistent. also check if the file already exists to avoid unnecessary requests
-        request_ids = []
+    def _set_request_ids(self, all_ids, force_new=False):
+        self.request_ids = []
         existing_files = 0
         for id in all_ids:
             id = self.convert_id(id)
@@ -142,42 +136,57 @@ class collection():
             if force_new == False and os.path.exists(f"{self._bag_data_folder}/{id}.city.json"):
                 existing_files += 1
             else:
-                request_ids.append(id)
+                self.request_ids.append(id)
 
         # check how many requests are needed
         if existing_files == len(all_ids):
             print("All the requested data is already saved on the machine, no new requests are needed.")
-            return
         elif existing_files > 0:
             print(f"{existing_files} out of {len(all_ids)} files already exist, so {len(all_ids) - existing_files} more request(s) are needed.")
 
-        # if asyncio is running, do the requests synchronously
-        if asyncio.get_event_loop().is_running(): #TODO: this doesn't work if you run the class twice
-            error_count = 0
-            if self._verbose:
-                print("Asyncio is already running, so the requests will be done synchronously. Note that this is not the most efficient way.")
-            for id in request_ids: # TODO: add a progress bar
-                try:
-                    r = requests.get(self._make_url(id))
-                    cityjson = self._convert_to_cityjson(r.json(), bag_attributes, id) #TODO: make it work properly with bag_attributes
-                    self._save(cityjson, id)
-                except:
-                    error_count += 1
-            if self._verbose:
-                print(f"{error_count} errors occurred while requesting the data.")
-        # if asyncio is not running, use the async function
-        else:
-            all_urls = [self._make_url(id) for id in request_ids]
-            bag_attributes = self._get_bag_attributes(request_ids)
-            self.result = asyncio.run(request_url_list(all_urls))
-            roof_attributes = self._get_roof_attributes(request_ids)
+    async def async_request_data(self):
+        all_urls = [self._make_url(id) for id in self.request_ids]
+        bag_attributes = self._get_bag_attributes(self.request_ids)
+        self.result = await request_url_list(all_urls)
+        roof_attributes = self._get_roof_attributes(self.request_ids)
 
-            # convert to the right format and save the data, this is not async because the data is already fetched
-            for id, data in zip(request_ids, self.result): # TODO: check if id corresponds to the id in the data
-                if data is None:
-                    continue
-                cityjson = self._convert_to_cityjson(data, bag_attributes, roof_attributes, id)
-                self._save(cityjson, id) # use request_ids to get the right id without the pre- and suffix
-            if self._verbose: #TODO: doesn't work anymore as we save all result
-                print(f"{len(all_urls)-len(self.result)} errors occurred while requesting the data.")
+        # convert to the right format and save the data, this is not async because the data is already fetched
+        for id, data in zip(self.request_ids, self.result): # TODO: check if id corresponds to the id in the data
+            if data is None:
+                continue
+            cityjson = self._convert_to_cityjson(data, bag_attributes, roof_attributes, id)
+            self._save(cityjson, id) # use request_ids to get the right id without the pre- and suffix
+        if self._verbose: #TODO: doesn't work anymore as we save all results
+            print(f"{len(all_urls)-len(self.result)} errors occurred while requesting the data.")
+
+    def _request_data(self):
+        #TODO: implement this function
+        error_count = 0
+        if self._verbose:
+            print("Asyncio is already running, so the requests will be done synchronously. Note that this is not the most efficient way.")
+        for id in self.request_ids: # TODO: add a progress bar
+            try:
+                r = requests.get(self._make_url(id))
+                cityjson = self._convert_to_cityjson(r.json(), bag_attributes, id) #TODO: make it work properly with bag_attributes
+                self._save(cityjson, id)
+            except:
+                error_count += 1
+        if self._verbose:
+            print(f"{error_count} errors occurred while requesting the data.")
+
+    def collect_id_list(self, all_ids: list, force_new=False):
+        """Requests and save the data in cityjson format for all the ids in the list."""
+
+        self._check_bag_data_folder()
+
+        # Remove the prefix from the ids so they are consistent. also check if the file already exists to avoid unnecessary requests
+        self._set_request_ids(all_ids, force_new)
+
+        # if asyncio is running, do the requests synchronously
+        if len(self.request_ids) and asyncio.get_event_loop().is_running(): #TODO: this doesn't work if you run the class twice
+            raise NotImplementedError("The async_request_data function cannot be called while asyncio is running.")
+        # if asyncio is not running, use the async function
+        elif len(self.request_ids):
+            asyncio.run(self.async_request_data())
+        # else: there is no data to request
         
